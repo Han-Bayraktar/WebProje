e2ee25b6aea6ef5f30916bddced4f73fa34799a8
Han-Bayraktar <beyaztbayraktar@gmail.com>
2023-05-21 15:28:40 +0300

    TAMAMLANDI
    iletişim sayfasına kayıtlı formlar için textbox ekledim ve js dosyasını değiştirdim ve proje bitti

--- iletisim.js
+++ iletisim.js
@@ -1,58 +1,62 @@
 new Vue({
   el: '#app',
   data: {
-    isim: '',
-    soyisim: '',
-    cinsiyet: '',
-    email: '',
-    telefon: '',
-    ilgialanlari: [],
-    ülke: '',
-    telefonerror: false
+    formData: {
+      isim: '',
+      soyisim: '',
+      cinsiyet: '',
+      email: '',
+      telefon: '',
+      ilgiAlanlari: [],
+      ulke: ''
+    },
+
   },
   methods: {
-    kaydetForm: function() {
-      console.log('İsim:', this.isim);
-      console.log('Soyadı:', this.soyisim);
-      console.log('Cinsiyet:', this.cinsiyet);
-      console.log('E-posta:', this.email);
-      console.log('Telefon Numarası:', this.telefon);
-      console.log('İlgi Alanları:', this.ilgialanlari);
-      console.log('Ülke:', this.ülke);
-
-      this.resetForm();
-      this.kaydetForm();
+    kaydetForm(event) {
+      event.preventDefault();
+    
+      const isim = document.getElementById('isim').value;
+      const soyisim = document.getElementById('soyisim').value;
+      const cinsiyet = document.querySelector('input[name="cinsiyet"]:checked').value;
+      const email = document.getElementById('email').value;
+      const telefon = document.getElementById('telefon').value;
+      const ilgiAlanlari = Array.from(document.getElementById('ilgi-alanlari').selectedOptions).map(option => option.value).join(', ');
+      const ulke = document.getElementById('country').value;
+    
+      const formDataText = `
+        isim: ${isim}
+        soyisim: ${soyisim}
+        cinsiyet: ${cinsiyet}
+        email: ${email}
+        telefon: ${telefon}
+        ilgiAlanlari: ${ilgiAlanlari}
+        ulke: ${ulke}
+      `;
+    
+      const formkayitlariTextarea = document.getElementById('formkayitlari');
+      formkayitlariTextarea.value += formDataText + '\n';
     },
-    resetForm: function() {
-      this.isim = '';
-      this.soyisim = '';
-      this.cinsiyet = '';
-      this.email = '';
-      this.telefon = '';
-      this.ilgialanlari = [];
-      this.ülke = '';
-      this.telefonerror = false;
-
-      this.resetForm();
+    resetForm() {
+      document.getElementById('isim').value = '';
+      document.getElementById('soyisim').value = '';
+      document.querySelector('input[name="cinsiyet"]:checked').checked = false;
+      document.getElementById('email').value = '';
+      document.getElementById('telefon').value = '';
+      Array.from(document.getElementById('ilgi-alanlari').options).forEach(option => option.selected = false);
+      document.getElementById('country').value = '';
     },
-    isNumber: function(event) {
-      if (event.keyCode < 48 || event.keyCode > 57) {
-        event.preventDefault();
+    validateTelefon(event) {
+      const input = event.target;
+      const phoneNumber = input.value.trim();
+      const numericRegex = /^\d{10}$/;
+      const warning = document.getElementById('telefonWarning');
+    
+      if (numericRegex.test(phoneNumber)) {
+        warning.style.display = 'none'; 
+      } else {
+        warning.style.display = 'block'; 
       }
-      this.telefonerror = this.telefon.length !== 10;
-    },
-    kaydetForm: function() {
-      var savedData = {
-        isim: this.isim,
-        soyisim: this.soyisim,
-        cinsiyet: this.cinsiyet,
-        email: this.email,
-        telefon: this.telefon,
-        ilgialanlari: this.ilgialanlari,
-        ülke: this.ülke
-      };
-
-      console.log('Form verileri kaydedildi:', savedData);
     }
   }
 });
